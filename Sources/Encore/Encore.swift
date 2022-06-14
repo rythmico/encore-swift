825ad5e4e38c93b49c03dd7906f48da4dcc314b1
David Roman <2538074+davdroman@users.noreply.github.com>
2022-06-14 13:27:42 +0100

    Initialize package

---
+++ Sources/Encore/Encore.swift
@@ -0,0 +1,6 @@
+public struct Encore {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/EncoreTests/EncoreTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Encore
+
+final class encore_swiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Encore().text, "Hello, World!")
+    }
+}
